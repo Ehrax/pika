df2731408644391497918b9ffb408bbcee8bb23e
Alexander Rasputin <rasputin.alex@hotmail.de>
2026-04-26 20:47:55 +0100

    Initial Commit

---
+++ pikaTests/pikaTests.swift
@@ -0,0 +1,16 @@
+//
+//  pikaTests.swift
+//  pikaTests
+//
+//  Created by Alexander Rasputin on 26.04.26.
+//
+
+import Testing
+
+struct pikaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
