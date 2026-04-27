17dc6540d5f2b00b29fcfe6576aa4104697662a9
Alexander Rasputin <rasputin.alex@hotmail.de>
2026-04-27 21:38:20 +0100

    Revert "fix(mac): persist secondary split width"
    This reverts commit 1a9f5d46c325f3c2666639880ecd60d52aa71a5b.

--- pika/Shell/ResizableDetailSplitView.swift
+++ pika/Shell/ResizableDetailSplitView.swift
@@ -3,13 +3,6 @@ import SwiftUI
 struct ResizableDetailSplitView<Leading: View, Detail: View>: View {
     private let leading: Leading
     private let detail: Detail
-    @SceneStorage("pika.resizableDetailSplit.leadingWidth") private var storedLeadingWidth = Self.defaultLeadingWidth
-    @State private var dragStartWidth: CGFloat?
-
-    private static var defaultLeadingWidth: Double { 500 }
-    private static var minimumLeadingWidth: CGFloat { 320 }
-    private static var maximumLeadingWidth: CGFloat { 620 }
-    private static var minimumDetailWidth: CGFloat { 460 }
 
     init(
         @ViewBuilder leading: () -> Leading,
@@ -21,21 +14,9 @@ struct ResizableDetailSplitView<Leading: View, Detail: View>: View {
 
     var body: some View {
         #if os(macOS)
-        GeometryReader { proxy in
-            let leadingWidth = clampedLeadingWidth(for: proxy.size.width)
-
-            HStack(spacing: 0) {
-                leading
-                    .frame(width: leadingWidth)
-
-                splitHandle(containerWidth: proxy.size.width)
-
-                detail
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-            }
-            .onAppear {
-                storedLeadingWidth = leadingWidth
-            }
+        HSplitView {
+            leading
+            detail
         }
         #else
         HStack(spacing: 0) {
@@ -44,40 +25,4 @@ struct ResizableDetailSplitView<Leading: View, Detail: View>: View {
         }
         #endif
     }
-
-    #if os(macOS)
-    private func splitHandle(containerWidth: CGFloat) -> some View {
-        Rectangle()
-            .fill(PikaColor.border)
-            .frame(width: 1)
-            .frame(maxHeight: .infinity)
-            .contentShape(Rectangle().inset(by: -4))
-            .gesture(
-                DragGesture(minimumDistance: 0)
-                    .onChanged { value in
-                        let startingWidth = dragStartWidth ?? clampedLeadingWidth(for: containerWidth)
-                        dragStartWidth = startingWidth
-                        storedLeadingWidth = Double(clamped(
-                            startingWidth + value.translation.width,
-                            containerWidth: containerWidth
-                        ))
-                    }
-                    .onEnded { _ in
-                        dragStartWidth = nil
-                    }
-            )
-    }
-
-    private func clampedLeadingWidth(for containerWidth: CGFloat) -> CGFloat {
-        clamped(CGFloat(storedLeadingWidth), containerWidth: containerWidth)
-    }
-
-    private func clamped(_ width: CGFloat, containerWidth: CGFloat) -> CGFloat {
-        let availableMaximum = max(
-            Self.minimumLeadingWidth,
-            min(Self.maximumLeadingWidth, containerWidth - Self.minimumDetailWidth)
-        )
-        return min(max(width, Self.minimumLeadingWidth), availableMaximum)
-    }
-    #endif
 }
